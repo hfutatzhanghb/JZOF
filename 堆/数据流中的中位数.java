7476647aee78b637f33166aa2c4ff116f9f3bdb9
hfutatzhanghb <1036798979@qq.com>
2019-06-01 08:41:38 +0800

    数据流中的中位数 git push https://github.com/hfutatzhanghb/JZOF.git
    git commit -m 数据流中的中位数

---
+++ 堆/数据流中的中位数.java
@@ -0,0 +1,38 @@
+package com.zhb.jzof;
+
+import java.util.PriorityQueue;
+import java.util.Comparator;
+public class 数据流中的中位数 {
+    public static void main(String[] args) {
+
+    }
+    private int count=0;
+    private PriorityQueue<Integer> minHeap=new PriorityQueue<>();
+    private PriorityQueue<Integer> maxHeap=new PriorityQueue<Integer>(11,
+            new Comparator<Integer>(){
+                public int compare(Integer o1,Integer o2){
+                    return o2-o1;
+                }
+            });
+    public void Insert(Integer num) {
+        if((count&1)==0){
+            maxHeap.offer(num);
+            int max=maxHeap.poll();
+            minHeap.offer(max);
+        }else{
+            minHeap.offer(num);
+            int min=minHeap.poll();
+            maxHeap.offer(min);
+        }
+        count++;
+    }
+
+    public Double GetMedian() {
+        if((count&1)==0) {
+            return new Double((minHeap.peek()+maxHeap.peek()))/2;
+        }else{
+            return new Double(minHeap.peek());
+        }
+    }
+
+}
