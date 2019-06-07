1d350eac66cacc46d60f7eafd1c30ce824996c78
hfutatzhanghb <1036798979@qq.com>
2019-06-07 08:16:26 +0800

    二叉树

---
+++ 二叉树/之字形打印二叉树.java
@@ -0,0 +1,71 @@
+package com.zhb.jzof;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class 之字形打印二叉树 {
+    public static void main(String[] args) {
+
+    }
+
+    class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+
+        }
+
+    }
+
+    public static ArrayList<ArrayList<Integer>> Print(TreeNode pRoot) {
+        int layer = 1;
+        //s1存奇数层节点
+        Stack<TreeNode> s1 = new Stack<TreeNode>();
+        s1.push(pRoot);
+        //s2存偶数层节点
+        Stack<TreeNode> s2 = new Stack<TreeNode>();
+
+        ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>();
+
+        while (!s1.empty() || !s2.empty()) {
+            if (layer % 2 != 0) {
+                ArrayList<Integer> temp = new ArrayList<Integer>();
+                while (!s1.empty()) {
+                    TreeNode node = s1.pop();
+                    //这里必须要判空，因为入栈时没有判断左右孩子是否为空
+                    if (node != null) {
+                        temp.add(node.val);
+                        System.out.print(node.val + " ");
+                        s2.push(node.left);
+                        s2.push(node.right);
+                    }
+                }
+                if (!temp.isEmpty()) {
+                    list.add(temp);
+                    layer++;
+                    System.out.println();
+                }
+            } else {
+                ArrayList<Integer> temp = new ArrayList<Integer>();
+                while (!s2.empty()) {
+                    TreeNode node = s2.pop();
+                    if (node != null) {
+                        temp.add(node.val);
+                        System.out.print(node.val + " ");
+                        s1.push(node.right);
+                        s1.push(node.left);
+                    }
+                }
+                if (!temp.isEmpty()) {
+                    list.add(temp);
+                    layer++;
+                    System.out.println();
+                }
+            }
+        }
+        return list;
+    }
+}
